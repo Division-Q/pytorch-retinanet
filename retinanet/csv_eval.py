1b9798836e969c34a66282ca1aa0ddf2875f164b
jonlee48 <jonathanlee@gwu.edu>
2022-04-18 19:56:50 -0400

    Done confusion matrix

--- retinanet/csv_eval.py
+++ retinanet/csv_eval.py
@@ -194,6 +194,7 @@ def evaluate(
             for d in detections:
                 scores = np.append(scores, d[4])
 
+                # if number of ground truth annotations is 0
                 if annotations.shape[0] == 0:
                     false_positives = np.append(false_positives, 1)
                     true_positives  = np.append(true_positives, 0)
@@ -260,3 +261,154 @@ def evaluate(
 
     return average_precisions
 
+
+def evaluate_confusion_matrix(
+        generator,
+        retinanet,
+        iou_threshold=0.5,
+        score_threshold=0.05,
+        max_detections=100,
+        save_path=None
+):
+    """ Evaluate a given dataset using a given retinanet.
+    # Arguments
+        generator       : The generator that represents the dataset to evaluate.
+        retinanet           : The retinanet to evaluate.
+        iou_threshold   : The threshold used to consider when a detection is positive or negative.
+        score_threshold : The score confidence threshold to use for detections.
+        max_detections  : The maximum number of detections to use per image.
+        save_path       : The path to save precision recall curve of each label.
+    # Returns
+        A dict mapping class names to mAP scores.
+    """
+
+    # gather all detections and annotations
+
+    all_detections = _get_detections(generator, retinanet, score_threshold=score_threshold,
+                                     max_detections=max_detections, save_path=save_path)
+    all_annotations = _get_annotations(generator)
+
+    average_precisions = {}
+
+    confusion_matrix = {}
+
+    for label in range(generator.num_classes()):
+        false_positives = np.zeros((0,))
+        true_positives = np.zeros((0,))
+
+        scores = np.zeros((0,))
+        num_annotations = 0.0
+
+        confusion_matrix[label] = {}
+        for i in range(generator.num_classes()):
+            confusion_matrix[label][i] = 0
+
+        for i in range(len(generator)):
+            detections = all_detections[i][label]
+            annotations = all_annotations[i][label]
+            num_annotations += annotations.shape[0]
+            detected_annotations = []
+
+            for d in detections:
+                scores = np.append(scores, d[4])
+
+                # if number of ground truth annotations is 0
+                if annotations.shape[0] == 0:
+                    false_positives = np.append(false_positives, 1)
+                    true_positives = np.append(true_positives, 0)
+                    continue
+
+                overlaps = compute_overlap(np.expand_dims(d, axis=0), annotations)
+                assigned_annotation = np.argmax(overlaps, axis=1)
+                max_overlap = overlaps[0, assigned_annotation]
+
+                # A positive detection that matches with a correct annotation that has not been detected already
+                # i.e. trying to avoid overlapping bounding boxes of the same class
+                if max_overlap >= iou_threshold and assigned_annotation not in detected_annotations:
+                    false_positives = np.append(false_positives, 0)
+                    true_positives = np.append(true_positives, 1)
+                    detected_annotations.append(assigned_annotation)
+                    confusion_matrix[label][label] += 1
+                # Otherwise it is treated as a false positive
+                else:
+                    false_positives = np.append(false_positives, 1)
+                    true_positives = np.append(true_positives, 0)
+
+                    # Within the false positives, we want to identify misclassifications
+                    #   When it matches with an annotation of another class
+
+                    # Check with all other classes for an overlap
+                    best_overlap = -1
+                    best_l = -1
+
+                    for l in range(generator.num_classes()):
+                        if l == label:
+                            continue
+
+                        annotations_l = all_annotations[i][l]
+
+                        if annotations_l.shape[0] == 0:
+                            continue
+
+                        overlaps_l = compute_overlap(np.expand_dims(d, axis=0), annotations_l)
+                        assigned_annotation_l = np.argmax(overlaps_l, axis=1)
+                        max_overlap_l = overlaps_l[0, assigned_annotation_l]
+
+                        if max_overlap_l >= iou_threshold:
+                            # keep track of the best overlap seen so far
+                            if max_overlap_l > best_overlap:
+                                best_overlap = max_overlap_l
+                                best_l = l
+
+                    # add a misclassification of label corresponding to class with most overlap
+                    if best_l != -1:
+                        confusion_matrix[label][best_l] += 1
+
+
+        # no annotations -> AP for this class is 0 (is this correct?)
+        if num_annotations == 0:
+            average_precisions[label] = 0, 0
+            continue
+
+        # sort by score
+        indices = np.argsort(-scores)
+        false_positives = false_positives[indices]
+        true_positives = true_positives[indices]
+
+        # compute false positives and true positives
+        false_positives = np.cumsum(false_positives)
+        true_positives = np.cumsum(true_positives)
+        # add correct classifications to the confusion matrix
+        #confusion_matrix[label][label] = true_positives
+
+        # compute recall and precision
+        recall = true_positives / num_annotations
+        precision = true_positives / np.maximum(true_positives + false_positives, np.finfo(np.float64).eps)
+
+        # compute average precision
+        average_precision = _compute_ap(recall, precision)
+        average_precisions[label] = average_precision, num_annotations
+
+    print('\nmAP:')
+    for label in range(generator.num_classes()):
+        label_name = generator.label_to_name(label)
+        print('{}: {}'.format(label_name, average_precisions[label][0]))
+        if (len(precision) > 0):
+            print("Precision: ", precision[-1])
+        if (len(recall) > 0):
+            print("Recall: ", recall[-1])
+
+        if save_path != None:
+            plt.plot(recall, precision)
+            # naming the x axis
+            plt.xlabel('Recall')
+            # naming the y axis
+            plt.ylabel('Precision')
+
+            # giving a title to my graph
+            plt.title('Precision Recall curve')
+
+            # function to show the plot
+            plt.savefig(save_path + '/' + label_name + '_precision_recall.jpg')
+
+    return average_precisions, confusion_matrix
